6f1ef75f070d76777c2e0676b3d1e3e2ada51a77
Edward Singleton <Edwardtonnn@gmail.com>
2023-04-23 02:54:04 -0700

    adds flag background on hover

--- script.js
+++ script.js
@@ -88,3 +88,32 @@ gsap.fromTo(countryLink,{
     ease:'power2.inOut',
     stagger:.1
 })
+
+// Changes bg 
+const containerMain = document.querySelector('.container-main');
+const listItemElements = document.querySelectorAll('ul li');
+
+listItemElements.forEach(li => {
+  li.addEventListener('mouseenter', () => {
+    const bgImage = li.getAttribute('data-bg');
+
+    // Fade in the new background image
+    gsap.to(containerMain, {
+      background: `url(${bgImage}) no-repeat center center`,
+      backgroundSize: 'cover',
+      duration: 0.5,
+      ease: 'power2.inOut',
+      opacity: 1
+    });
+  });
+
+  li.addEventListener('mouseleave', () => {
+    // Reset the background when the user stops hovering
+    gsap.to(containerMain, {
+      background: '',
+      duration: 0.5,
+      ease: 'power2.inOut',
+      opacity: 1
+    });
+  });
+});
