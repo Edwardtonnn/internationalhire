af3cd93f451335103085609c3d5ac6556365f95f
Edward Singleton <Edwardtonnn@gmail.com>
2023-04-23 01:29:29 -0700

    adds updating title and submit button

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 
 
 let countryLink = document.querySelectorAll('ul a');
+let countryTitle = document.getElementsByClassName('titleText');
+
+function resetInput(){
+    setTimeout(()=>{
+        document.querySelectorAll("form input, form textarea").forEach((input)=>{
+            input.value = "";
+        })
+
+    }, 2000)
+}
+
+
 
 countryLink.forEach(link => {
     link.addEventListener('click',()=>{
@@ -14,10 +26,31 @@ countryLink.forEach(link => {
 
         })
     })
+
+    link.addEventListener('click',()=>{
+
+       countryTitle[0].innerText = link.innerText;
+    })
 });
 
 let closeIcon = document.querySelector('span.close');
+let submitBtn = document.querySelector('button');
 
+submitBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    gsap.to('.container-form',{
+        opacity:0,        
+        duration:0.25,
+        ease:'power2.out'
+    })
+    gsap.to('.container-form',{
+        display:'none',        
+        duration:0.25,
+        
+    })
+
+    resetInput();
+})
 closeIcon.addEventListener('click',()=>{
     gsap.to('.container-form',{
         opacity:0,        
@@ -29,6 +62,8 @@ closeIcon.addEventListener('click',()=>{
         duration:0.25,
         
     })
+
+    resetInput();
 })
 
 gsap.fromTo("h1, p",{
